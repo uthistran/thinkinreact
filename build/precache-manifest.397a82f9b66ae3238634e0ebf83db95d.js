5af922295b46fdbcf08afb97cdc7c545aa3ba1f8
Uthistran <uthistran@gmail.com>
2020-05-14 10:46:18 -0700

    Completing the example and adding build folder

---
+++ build/precache-manifest.397a82f9b66ae3238634e0ebf83db95d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b4294c1727531ef55e06ae12fefa9a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1574c3ffb69946dcb74a",
+    "url": "/static/js/2.94fc76d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.94fc76d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c29f8ddfffff11abe0f3",
+    "url": "/static/js/main.58b11d5a.chunk.js"
+  },
+  {
+    "revision": "9d4d8433e5014c49d098",
+    "url": "/static/js/runtime-main.eecb8184.js"
+  }
+]);
